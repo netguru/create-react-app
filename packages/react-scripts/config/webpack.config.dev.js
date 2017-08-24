a781d53e59cd9f256fe1209af4ecc455d3ad699e
Dominik Rowicki <dominik.rowicki@netguru.co>
2017-08-24 13:05:56 +0200

    Fix syntax error in Webpack config from a merge conflict

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -223,9 +223,9 @@ module.exports = {
                     }),
                   ],
                 },
+              },
               {
                 loader: require.resolve('sass-loader'),
-              },
               },
             ],
           },
