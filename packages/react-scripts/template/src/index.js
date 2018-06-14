6819f4564b9c000ce4514e58de2fbb300ee59f39
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-06-14 11:24:00 +0200

    Bump react-hot-loader version to 4

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import { hot } from 'react-hot-loader';
 import 'normalize.css';
 
 import App from 'components/App';
 import registerServiceWorker from 'utils/registerServiceWorker';
 
-function render(Component) {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('root'),
-  );
-}
+const HotApp = hot(module)(App);
 
-render(App);
-
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    // eslint-disable-next-line global-require
-    const nextApp = require('./components/App').default;
-    render(nextApp);
-  });
-}
+ReactDOM.render(<HotApp />, document.getElementById('root'));
 
 registerServiceWorker();
